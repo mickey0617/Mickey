e65951d4413a2249aa40199e0a9136127c38e2d3
Delfa0617 <160857953+Delfa0617@users.noreply.github.com>
2024-08-04 15:57:12 +0100

    Update cmds

---
+++ scripts/cmds/salut.js
@@ -0,0 +1,20 @@
+module.exports = {
+    config: {
+        name: "salut",
+        version: "1.0",
+        author: "kivv",
+        countDown: 5,
+        role: 0,
+        shortDescription: "No Prefix",
+        longDescription: "No Prefix",
+        category: "reply",
+    },
+onStart: async function(){}, 
+onChat: async function({
+    event,
+    message,
+    getLang
+}) {
+    if (event.body && event.body.toLowerCase() == "salut") return message.reply("💌 𝐒𝐚𝐥𝐮𝐭 𝐮𝐭𝐢𝐥𝐢𝐬𝐚𝐭𝐞𝐮𝐫 𝐝𝐮 𝐛𝐨𝐭 🙋 , 𝐜𝐨𝐦𝐦𝐞𝐧𝐭 𝐯𝐚𝐬 𝐭𝐮  ? T𝐚𝐩é { ~𝐡𝐞𝐥𝐩 } 𝐩𝐨𝐮𝐫 𝐯𝐨𝐢𝐫 𝐦𝐞𝐬 𝐜𝐨𝐦𝐦𝐚𝐧𝐝𝐞 🤖");
+}
+};
